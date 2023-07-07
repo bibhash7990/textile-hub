17536538678031b0a32401527c17197f733ae86a
bibhash <bibhash88lenka@gmail.com>
2023-07-07 20:24:06 +0530

    adding signup and signin

---
+++ src/component/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
+import "./sign-up-form.style.scss";
+
+import {
+  createAuthUserWithEmailAndPassword,
+  createuserDocumentFromAuth,
+} from "../../utils/firebase/firebase.util";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const SignUpForm = () => {
+  const [formFeilds, setFormFeilds] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFeilds;
+
+  console.log(formFeilds);
+
+  const resetFormFields = () => {
+    setFormFeilds(defaultFormFields);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("passwords do not match");
+      return;
+    }
+
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await createuserDocumentFromAuth(user, { displayName });
+      resetFormFields();
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        alert("This Email is already in use");
+      } else {
+        console.log("user creation encountered an error", error);
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFeilds({ ...formFeilds, [name]: value });
+  };
+
+  return (
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Display Name"
+          type="text"
+          required
+          onChange={handleChange}
+          name="displayName"
+          value={displayName}
+        />
+
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+
+        <FormInput
+          label="Confirm Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
+
+        <Button type="submit">Sign Up</Button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,11 +1,29 @@
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
 import {
+  // auth,
+  // signInWithGoogleRedirect,
   signInWithGooglePopup,
   createuserDocumentFromAuth,
 } from "../../utils/firebase/firebase.util";
 
+import SignUpForm from "../../component/sign-up-form/sign-up-form.component";
+
 const SignIn = () => {
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await getRedirectResult(auth);
+  //     if (!response) {
+  //       // eslint-disable-next-line no-unused-vars
+  //       const userDocRef = await createuserDocumentFromAuth(response.user);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
+    // eslint-disable-next-line no-unused-vars
     const userDocRef = await createuserDocumentFromAuth(user);
   };
 
@@ -13,6 +31,10 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
+      {/* <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button> */}
     </div>
   );
 };
